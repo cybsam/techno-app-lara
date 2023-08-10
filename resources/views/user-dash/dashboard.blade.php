7740c71b1e4270741e2cf942b1c217045b80ce6c
cybsam <samshuvo00@gmail.com>
2023-08-10 12:46:44 +0600

    aug:10 upd:dsh~usr&%

--- resources/views/user-dash/dashboard.blade.php
+++ resources/views/user-dash/dashboard.blade.php
@@ -2,7 +2,9 @@
 @section('title', 'Fire Panel Repair & Maintenance BD - Techno Apogee Limited')
 @section('content')
 
-
+<h1 class="float-center">
+    {{ Auth::user()->name }}
+</h1>
 
 
 @endsection
