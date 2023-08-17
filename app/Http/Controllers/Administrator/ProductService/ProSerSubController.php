2b841c59ad5f4d5a67a344d0966d0900380264b6
cybsam <samshuvo00@gmail.com>
2023-08-17 12:14:58 +0600

    aug:17 add:SubProductService #rte

---
+++ app/Http/Controllers/Administrator/ProductService/ProSerSubController.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Http\Controllers\Administrator\ProductService;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Carbon\Carbon;
+use Image;
+use Illuminate\Support\Str;
+use App\Models\ProductService;
+
+class ProSerSubController extends Controller
+{
+    //
+}
