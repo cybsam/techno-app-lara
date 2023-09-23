03d7d466c349ff5e893c65bec47a81e434ec8e33
cybsam <samshuvo00@gmail.com>
2023-09-23 14:13:31 +0600

    spdgrti Trap Lort

--- resources/views/FrontEndView/layouts/inc/header.blade.php
+++ resources/views/FrontEndView/layouts/inc/header.blade.php
@@ -232,7 +232,7 @@
                         <li><a href="{{ route('frontEndIndex.ProductAndService',['slug'=>'electrical-solution']) }}">ELECTRICAL SOLUTION</a></li>
                         <li><a href="{{ route('frontEndIndex.ProductAndService',['slug'=>'fire-solution']) }}">FIRE SOLUTION</a></li>
                         <li><a href="{{ route('frontEndIndex.ProductAndService',['slug'=>'automation-solution']) }}">AUTOMATION SOLUTION</a></li>
-
+                        <li><a href="{{ route('frontEndIndex.strategic-partners') }}">Strategic Partners</a></li>
                     </ul>
                 </li>
                 <li>
